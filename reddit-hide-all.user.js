2e1ae65471b8ca7d47fbc94ffa2cc09f38afea86
Bryan Creel <scytalezero@users.noreply.github.com>
2016-07-22 16:24:08 -0400

    Fixes for reddit

--- reddit-hide-all.user.js
+++ reddit-hide-all.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Reddit Hide All
 // @namespace     http://ligature.me
-// @version       1.0.0
+// @version       1.0.1
 // @description   Adds a button to Reddit that hides all posts on the current screen.
 // @homepageURL   https://github.com/scytalezero/reddit-hide-all
 // @updateURL     https://github.com/scytalezero/reddit-hide-all/raw/master/reddit-hide-all.user.js
@@ -12,16 +12,32 @@
 // @grant         none
 // ==/UserScript==
 
-if ($(".hide-button").length > 0) {
-  $("#header-bottom-right")
-    .prepend(`<span class="separator">|</span>`)
-    .prepend($("<a>hide page</a>")
-      .attr({
-        "href": "#",
-        "id": "hide-all"
-      })
-      .click(() => {
-        $(".hide-button a").trigger("click")
-      })
-    )	
-}
+/* jshint asi: true, esnext: true */
+(function() {
+  'use strict'
+  
+  //Reddit seems to be rate-limited or something. A 400ms delay seems to work.
+  function hidePump(elements) {
+    if (elements.length > 0) {
+      elements.shift().click()
+      setTimeout(() => hidePump(elements), 400)
+    }
+  }
+  
+  if ($(".hide-button").length > 0) {
+    const $hideButton = $("<a>hide page</a>")
+        .attr({
+          "href": "#",
+          "id": "hide-all"
+        })
+        .click(function() {
+          hidePump($(".hide-button a").toArray())
+        })
+    $("#header-bottom-right")
+      .prepend(`<span class="separator">|</span>`)
+      .prepend($hideButton.clone(true))
+    $(".nextprev")
+      .append($hideButton)
+  }
+
+})()
